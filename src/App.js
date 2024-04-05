d059a108894f40d91b99b45a465765e8896c433f
tetteh victor <victortetteh520@gmail.com>
2024-04-05 13:00:02 +0100

    done again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Counter from './components/Counter'
-
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Section from './components/Section'
 const App = () => {
   return (
     <>
     <Counter/>
+    <Header/>
+    <Navbar/>
+    <Section/>
     </>
   )
 }
---
+++ src/components/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section = () => {
+  return (
+    <>
+    <h1>sections</h1>
+    </>
+  )
+}
+
+export default Section
